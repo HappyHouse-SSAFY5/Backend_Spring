6058b7a6b5e6d357a0a1383e29577712a1b210ba
dldptmf0602 <dldptmf0602@naver.com>
2021-05-26 09:05:31 +0900

    update: rename and removed unuse route

--- src/main/java/com/ssafy/happyhouse/model/mapper/PickedHouseMapper.java
+++ src/main/java/com/ssafy/happyhouse/model/mapper/PickedHouseMapper.java
@@ -12,9 +12,6 @@ public interface PickedHouseMapper {
 	// 찜 목록 출력
 	public List<PickedHouseDto> pickedHouseList(String userid)throws SQLException;
 	
-	// 찜 한 것 상세 보기
-	public PickedHouseDto pickedHouseDetail(int pickedId)throws SQLException;
-	
 	// 찜하기
 	public int pick(Map<String, String> map) throws SQLException;
 	
--- src/main/java/com/ssafy/happyhouse/model/service/PickedHouseService.java
+++ src/main/java/com/ssafy/happyhouse/model/service/PickedHouseService.java
@@ -10,9 +10,6 @@ public interface PickedHouseService {
 	// 찜 목록 출력
 	public List<PickedHouseDto> pickedHouseList(String userid)throws SQLException;
 	
-	// 찜 한 것 상세 보기
-	public PickedHouseDto pickedHouseDetail(int pickedId)throws SQLException;
-	
 	// 찜하기
 	public int pick(Map<String, String> map) throws SQLException;
 	
